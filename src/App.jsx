1dd5b02970ef8c8818bfaf50eb193d8faad0567b
Morgan Byrd <morganthemosaic@gmail.com>
2022-07-13 19:33:46 -0400

    Connecting App to main

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+const App = () => {
+    return (
+        <p>Test</p>
+    )
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import App from "./App.jsx"
+import ReactDOM from "react-dom/client";
+
+const container = document.querySelector("#root");
+const root = ReactDOM.createRoot(container);
+
+root.render(<App />)
