1e65b3b894d209dd814f6fa36454e6ecd783ed96
Morgan Byrd <morganthemosaic@gmail.com>
2022-07-13 20:55:14 -0400

    Add text and background image to WelcomeBanner

--- src/WelcomeBanner.jsx
+++ src/WelcomeBanner.jsx
@@ -1,7 +1,12 @@
+import styles from "./styles/WelcomeBanner.module.css";
+
 const WelcomeBanner = () => {
-    return (
-        <p>Welcome to Mosaic Movies</p>
-    )
-}
+	return (
+		<div className={styles.welcomeBanner}>
+			<h1>WELCOME TO MOSAIC MOVIES</h1>
+			<h3>See the world through a different lens.</h3>
+		</div>
+	);
+};
 
 export default WelcomeBanner;
