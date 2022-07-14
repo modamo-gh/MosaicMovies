e7f93737c37bcbd7de82b2ae06fc674c95f29627
Morgan Byrd <morganthemosaic@gmail.com>
2022-07-14 19:27:54 -0400

    Add movie posters into NowShowing

--- src/components/NowShowing.jsx
+++ src/components/NowShowing.jsx
@@ -1,7 +1,19 @@
-import styles from "../styles/NowShowing.module.css"
+import atlantisPoster from "../images/2001-atlantis_the_lost_empire-2-1.jpg";
+import herPoster from "../images/her-poster-joaquin-phoenix-1.jpg";
+import intoTheWildPoster from "../images/into-the-wild-poster-1.jpg";
+import meetTheRobinsonsPoster from "../images/2007-meet_the_robinsons-1-1.jpg";
+import riseOfThePlanetOfTheApesPoster from "../images/rise-of-the-apes-uk-poster-1.jpg"
+import styles from "../styles/NowShowing.module.css";
+
 const NowShowing = () => {
     return (
-        <p className={styles.nowShowing}>Now Showing</p>
+        <div className={styles.nowShowing}>
+            <img src={herPoster} />
+            <img src={meetTheRobinsonsPoster} />
+            <img src={intoTheWildPoster} />
+            <img src={atlantisPoster} />
+            <img src={riseOfThePlanetOfTheApesPoster} />
+        </div>
     )
 }
 
