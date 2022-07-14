b35b8938eadc7a77f35c3d084417be25169ed12d
Morgan Byrd <morganthemosaic@gmail.com>
2022-07-14 18:50:34 -0400

    Add links to Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Navbar from "./Navbar.jsx";
-import NowShowing from "./NowShowing";
-import WelcomeBanner from "./WelcomeBanner.jsx";
+import Navbar from "./components/Navbar.jsx";
+import NowShowing from "./components/NowShowing";
+import WelcomeBanner from "./components/WelcomeBanner.jsx";
 import styles from "./styles/App.module.css"
 
 const App = () => {
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,27 @@
 import styles from "../styles/Navbar.module.css";
 const Navbar = () => {
     return (
-        <p className={styles.navbar}>Navbar</p>
-    )
+		<nav className={styles.navbar}>
+			<ul className={styles.links}>
+				<a href="#">
+					<li>Home</li>
+				</a>
+				<a href="#">
+					<li>Now Showing</li>
+				</a>
+				<a href="#">
+					<li>Coming Soon</li>
+				</a>
+				<a href="#">
+					<li>Concessions</li>
+				</a>
+				<a href="#">
+					<li>Contact Us</li>
+				</a>
+				<li>Avatar</li>
+			</ul>
+		</nav>
+	);
 }
 
 export default Navbar;
