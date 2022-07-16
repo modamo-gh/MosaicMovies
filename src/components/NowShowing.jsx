adf458ad16d9162e37340c63f8cb54f9e25f7d19
Morgan Byrd <morganthemosaic@gmail.com>
2022-07-16 03:07:35 -0400

    Moved posters to poster constant

--- src/components/NowShowing.jsx
+++ src/components/NowShowing.jsx
@@ -1,18 +1,14 @@
-import atlantisPoster from "../images/2001-atlantis_the_lost_empire-2-1.jpg";
-import herPoster from "../images/her-poster-joaquin-phoenix-1.jpg";
-import intoTheWildPoster from "../images/into-the-wild-poster-1.jpg";
-import meetTheRobinsonsPoster from "../images/2007-meet_the_robinsons-1-1.jpg";
-import riseOfThePlanetOfTheApesPoster from "../images/rise-of-the-apes-uk-poster-1.jpg"
+import posters from "../images/posters.js"
 import styles from "../styles/NowShowing.module.css";
 
 const NowShowing = () => {
     return (
         <div className={styles.nowShowing}>
-            <img src={herPoster} />
-            <img src={meetTheRobinsonsPoster} />
-            <img src={intoTheWildPoster} />
-            <img src={atlantisPoster} />
-            <img src={riseOfThePlanetOfTheApesPoster} />
+            <img src={posters.her} />
+            <img src={posters.meetTheRobinsons} />
+            <img src={posters.intoTheWild} />
+            <img src={posters.atlantis} />
+            <img src={posters.riseOfThePlanetOfTheApesPoster} />
         </div>
     )
 }
