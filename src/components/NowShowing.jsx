1b2416372be0f05f78d5b5df8ea15ef60bb9e051
Morgan Byrd <morganthemosaic@gmail.com>
2022-07-22 00:15:55 -0400

    Posters now generated using an array map

--- src/components/NowShowing.jsx
+++ src/components/NowShowing.jsx
@@ -1,34 +1,20 @@
 import MoviePoster from "./MoviePoster.jsx";
 import nowShowingStyles from "../styles/NowShowing.module.css";
 import posters from "../images/posters.js";
+
 const NowShowing = () => {
 	return (
 		<div className={nowShowingStyles.nowShowing}>
-			<MoviePoster
-				imageSource={posters.her.imageSource}
-				title={posters.her.title}
-				runtime={posters.her.runtime}
-			/>
-			<MoviePoster
-				imageSource={posters.meetTheRobinsons.imageSource}
-				title={posters.meetTheRobinsons.title}
-				runtime={posters.meetTheRobinsons.runtime}
-			/>
-			<MoviePoster
-				imageSource={posters.intoTheWild.imageSource}
-				title={posters.intoTheWild.title}
-				runtime={posters.intoTheWild.runtime}
-			/>
-			<MoviePoster
-				imageSource={posters.atlantis.imageSource}
-				title={posters.atlantis.title}
-				runtime={posters.atlantis.runtime}
-			/>
-			<MoviePoster
-				imageSource={posters.riseOfThePlanetOfTheApes.imageSource}
-				title={posters.riseOfThePlanetOfTheApes.title}
-				runtime={posters.riseOfThePlanetOfTheApes.runtime}
-			/>
+			{posters.map((poster) => {
+				return (
+					<MoviePoster
+						imageSource={poster.imageSource}
+						title={poster.title}
+						runtime={poster.runtime}
+						key={poster.title}
+					/>
+				);
+			})}
 		</div>
 	);
 };
--- src/images/posters.js
+++ src/images/posters.js
@@ -4,32 +4,32 @@ import intoTheWildPoster from "./into-the-wild-poster-1.jpg";
 import meetTheRobinsonsPoster from "./2007-meet_the_robinsons-1-1.jpg";
 import riseOfThePlanetOfTheApesPoster from "./rise-of-the-apes-uk-poster-1.jpg";
 
-const posters = {
-	atlantis: {
+const posters = [
+	{
 		imageSource: atlantisPoster,
 		runtime: "1hr 35m",
 		title: "Atlantis: The Lost Empire",
 	},
-	her: {
+	{
 		imageSource: herPoster,
 		runtime: "2hr 6m",
 		title: "Her",
 	},
-	intoTheWild: {
+	{
 		imageSource: intoTheWildPoster,
 		runtime: "2hr 28m",
 		title: "Into the Wild",
 	},
-	meetTheRobinsons: {
+	{
 		imageSource: meetTheRobinsonsPoster,
 		runtime: "1hr 35m",
 		title: "Meet the Robinsons",
 	},
-	riseOfThePlanetOfTheApes: {
+	{
 		imageSource: riseOfThePlanetOfTheApesPoster,
 		runtime: "1hr 45m",
 		title: "Rise of the Planet of the Apes"
 	}
-};
+];
 
 export default posters;
